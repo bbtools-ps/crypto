cac7cf8ada101944329f4d49bcdfb70e3c9b32a1
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2023-01-14 23:51:20 +0100

    styling fix

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = () => {
           sx={{
             display: "flex",
             width: "100%",
-            height: "100%",
+            minHeight: "100%",
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "flex-start",
