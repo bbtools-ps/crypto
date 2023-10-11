28a0565a5caf2293583211c6bc7b28d6a9477c32
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-10-11 23:34:07 +0200

    returned the hash

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -5,32 +5,32 @@ describe("main menu", () => {
 
     cy.get('[data-cy="ciphers-dropdown-menu"]').click();
     cy.get('[data-cy="caesar-btn"]').click();
-    cy.location("pathname").should("eq", "/caesar-cipher");
+    cy.location("hash").should("eq", "#/caesar-cipher");
     cy.get("h4").contains("Caesar Cipher");
 
     cy.get('[data-cy="ciphers-dropdown-menu"]').click();
     cy.get('[data-cy="vigenère-btn"]').click();
-    cy.location("pathname").should("eq", "/vigenere-cipher");
+    cy.location("hash").should("eq", "#/vigenere-cipher");
     cy.get("h4").contains("Vigenère Cipher");
 
     cy.get('[data-cy="ciphers-dropdown-menu"]').click();
     cy.get('[data-cy="emoji-btn"]').click();
-    cy.location("pathname").should("eq", "/emoji-cipher");
+    cy.location("hash").should("eq", "#/emoji-cipher");
     cy.get("h4").contains("Emoji Cipher");
 
     cy.get('[data-cy="about-btn"]').click();
-    cy.location("pathname").should("eq", "/about");
+    cy.location("hash").should("eq", "#/about");
     cy.get("h4").contains("About");
 
     cy.get('[data-cy="home-btn"]').click();
-    cy.location("pathname").should("eq", "/");
+    cy.location("hash").should("eq", "#/");
     cy.get("h4").contains("Emoji Cipher");
 
     cy.get('[data-cy="theme-switch-btn"]').click();
-    cy.location("pathname").should("eq", "/");
+    cy.location("hash").should("eq", "#/");
     cy.get("h4").contains("Emoji Cipher");
     cy.get('[data-cy="theme-switch-btn"]').click();
-    cy.location("pathname").should("eq", "/");
+    cy.location("hash").should("eq", "#/");
     cy.get("h4").contains("Emoji Cipher");
   });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { StyledEngineProvider } from "@mui/material/styles";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StyledEngineProvider injectFirst>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StyledEngineProvider>
 );
