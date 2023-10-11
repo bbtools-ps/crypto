c4fe3514a31d4f5205be65871df4ef461de6d13b
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-10-11 23:29:18 +0200

    removed hash router, updated tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { StyledEngineProvider } from "@mui/material/styles";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StyledEngineProvider injectFirst>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StyledEngineProvider>
 );
