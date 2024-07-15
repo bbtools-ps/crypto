4f2cc99d70c39d496f8d0fde8d9db3e2d4df625e
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-07-15 21:48:29 +0200

    imports cleanup, converted let to const

--- src/screens/caesar-cipher/CaesarCipher.test.tsx
+++ src/screens/caesar-cipher/CaesarCipher.test.tsx
@@ -1,6 +1,6 @@
+import { caesarEncryptDecrypt } from "@/utils/ciphers";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { caesarEncryptDecrypt } from "../../utils/ciphers";
 import { Component as CaesarCipher } from "./CaesarCipher";
 
 describe("<CaesarCipher/>", () => {
