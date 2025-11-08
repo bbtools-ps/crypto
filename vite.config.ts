7b8dc7d7651495c078fcc922d0b8b9d63b47a12e
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2025-11-08 14:09:36 +0100

    add vite-plugin-babel and babel-plugin-react-compiler for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "app"),
